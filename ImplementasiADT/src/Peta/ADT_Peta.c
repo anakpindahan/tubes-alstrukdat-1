d56e3fe46b327834bc317a87686fc3d2d1ab9855
Ardovigus <68460784+Ardovigus@users.noreply.github.com>
2020-11-28 01:58:13 +0700

    Add files via upload

--- ImplementasiADT/src/Peta/ADT_Peta.c
+++ ImplementasiADT/src/Peta/ADT_Peta.c
@@ -1,14 +1,26 @@
-#define Map_X_Max 9
+/*#define Map_X_Max 9
 #define Map_X_Min 1
 #define Map_Y_Max 9
 #define Map_Y_Min 1
 #define Ukuran_Peta 11
-
+*/
 
 typedef struct {
-	char Isi[Ukuran_Peta][Ukuran_Peta];
+	char Isi[50][50];
+	int NKolEff;
+	int NBrsEff;
 } Peta;
 
+typedef struct {
+    int Map_X_Max;
+    int Map_X_Min;
+    int Map_Y_Max;
+    int Map_Y_Min;
+} IDPetaUmum;
+
+IDPetaUmum Identitas_Peta;
+
+/*
 void bacaPeta(char* file_name, Peta* kontainer_peta) {
     char ch; int i,j;
     FILE* filePointer = fopen(file_name, "r");
@@ -26,6 +38,42 @@ void bacaPeta(char* file_name, Peta* kontainer_peta) {
             }
             ch = fgetc(filePointer);
         }
+    }
+
+    fclose(filePointer);
+}
+*/
+
+void bacaPeta(char* file_name, Peta* kontainer_peta) {
+    char ch; int i,j;
+    FILE* filePointer = fopen(file_name, "r");
+
+    if (filePointer == NULL) {
+        printf("File tidak ditemukan\n");
+    }
+    else {
+        /*
+        i = 0;
+        for (i; i < Ukuran_Peta; i++) {
+            j = 0;
+            for (j; j < Ukuran_Peta; j++) {
+                ch = fgetc(filePointer);
+                kontainer_peta->Isi[i][j] = ch;
+            }
+            ch = fgetc(filePointer);
+        }*/
+        i = 0; j = 0;
+        do {
+            ch = fgetc(filePointer);
+            if (ch != '\n' && ch != EOF) {
+                kontainer_peta->Isi[i][j] = ch;
+                j++;
+            } else if (ch == '\n') {
+                i++; j = 0;
+            }
+        } while (ch != EOF);
+        kontainer_peta->NBrsEff = i + 1;
+        kontainer_peta->NKolEff = j;
     }
 
     fclose(filePointer);
