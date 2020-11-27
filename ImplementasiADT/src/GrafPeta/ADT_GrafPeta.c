724ecd6b590dd6cdbc928d8ad55bb9f90ed0a625
Ardovigus <68460784+Ardovigus@users.noreply.github.com>
2020-11-28 01:11:34 +0700

    Add files via upload

--- ImplementasiADT/src/GrafPeta/ADT_GrafPeta.c
+++ ImplementasiADT/src/GrafPeta/ADT_GrafPeta.c
@@ -1,3 +1,12 @@
+//#ifndef _ADT_GRAFPETA_H
+//#define _ADT_GRAFPETA_H
+//#include "ADT_Peta.c"
+// _ADT_GRAFPETA_H
+
+#include <stdlib.h>
+//#include <stdio.h>
+//#define NULL NULL
+/*
 typedef struct {
     Peta* TargetKuadranPeta;
     Peta* AsalKuadranPeta;
@@ -9,4 +18,124 @@ typedef struct {
 } GrafPeta;
 
 GrafPeta GrafUntukPeta;
+*/
 
+typedef struct tGrafNode * adrGrafNode;
+typedef struct tGrafSuccNode * adrGrafSuccNode;
+
+typedef struct tGrafNode {
+	Peta* ID;
+	adrGrafSuccNode Trail;
+	adrGrafNode Next;
+} GrafNode;
+
+typedef struct tGrafSuccNode {
+	adrGrafNode Succ;
+	char arah;
+	adrGrafSuccNode Next;
+} GrafSuccNode;
+
+typedef struct {
+	adrGrafNode First;
+} Graph;
+
+Graph GrafPeta;
+
+adrGrafNode GrafAlokNode (Peta* peta) {
+    adrGrafNode Pn = (adrGrafNode) malloc (sizeof(GrafNode));
+
+    if (Pn != NULL) {
+        Pn->ID = peta;
+        Pn->Trail = NULL;
+        Pn->Next = NULL;
+    }
+    return Pn;
+}
+
+void GrafDealokNode (adrGrafNode Pn) {
+    free(Pn);
+}
+
+adrGrafSuccNode GrafAlokSuccNode (adrGrafNode Pn, char orientasi) {
+    adrGrafSuccNode Pt = (adrGrafSuccNode) malloc (sizeof(GrafSuccNode));
+
+    if (Pt != NULL) {
+        Pt->Succ = Pn;
+        Pt->Next = NULL;
+        Pt->arah = orientasi;
+    }
+    return Pt;
+}
+
+void GrafDealokSuccNode (adrGrafSuccNode Pt) {
+    free(Pt);
+}
+
+void GrafInsertNode (Graph G, Peta* peta) {
+    adrGrafNode Pn = GrafAlokNode(peta);
+
+    if (Pn != NULL) {
+        adrGrafNode Ptemp = G.First;
+
+        while (Ptemp->Next != NULL) {
+
+            Ptemp = Ptemp->Next;
+
+        }
+
+        Ptemp->Next = Pn;
+
+    }
+}
+
+void GrafInsertEdge (Graph G, Peta* prec, Peta* succ, char orientasi) {
+    adrGrafNode Pnprec = G.First;
+    adrGrafNode Pnsucc = G.First;
+
+    while (Pnprec->ID != prec) {
+        Pnprec = Pnprec->Next;
+    }
+
+    while (Pnsucc->ID != succ) {
+        Pnsucc = Pnsucc->Next;
+    }
+
+    adrGrafSuccNode Pt = Pnprec->Trail;
+
+    if (Pt == NULL) {
+        adrGrafSuccNode Pttemp = GrafAlokSuccNode(Pnsucc,orientasi);
+
+        if (Pttemp != NULL) {
+            Pnprec->Trail = Pttemp;
+        }
+    } else {
+        while (Pt->Next != NULL) {
+            Pt = Pt->Next;
+        }
+        adrGrafSuccNode Pttemp = GrafAlokSuccNode(Pnsucc,orientasi);
+
+        if (Pttemp != NULL) {
+            Pt->Next = Pttemp;
+        }
+    }
+}
+
+Peta* GrafSearchTarget(Graph G, Peta* peta_asal, char orientasi) {
+    adrGrafNode Pn = G.First;
+
+    while (Pn->ID != peta_asal) {
+        Pn = Pn->Next;
+    }
+
+    adrGrafSuccNode Pt = Pn->Trail;
+
+    while (Pt->arah != orientasi) {
+        Pt = Pt->Next;
+    }
+
+    adrGrafNode Target = Pt->Succ;
+
+    return Target->ID;
+}
+
+//#endif
